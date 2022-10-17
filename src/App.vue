5b582e6c0b77cb8a054777b82048931c72bcd436
abbb1399 <abbb123@naver.com>
2022-10-17 21:54:48 +0900

    App Component Header 및 transition css 추가

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <the-header />
+
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component" />
+    </transition>
+  </router-view>
 </template>
 
+<script>
+import TheHeader from './components/layout/TheHeader.vue'
+
+export default {
+   components:{
+    TheHeader,
+  },
+}
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  // Vue Transition css
+  .route-enter-from{
+    opacity: 0;
+    transform: translateY(-30px);
   }
-}
+
+  .route-leave-to{
+    opacity: 0;
+    transform: translateY(30px);
+  }
+
+  .route-enter-active{
+    transition: all 0.3s ease-out;
+  }
+
+  .route-leave-active{
+    transition: all 0.3s ease-in;
+  }
+
+  .route-enter-to,
+  .route-leave-from{
+    opacity: 1;
+    transform: translateY(0);
+  }
 </style>
+
